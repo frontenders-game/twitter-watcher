98bcbb1c1aff872ccb4b406b0488190a027537e1
frontenders-game <sounnecessary83@gmail.com>
2025-01-10 07:30:16 +0300

    first commit

---
+++ src/TwitterService.js
@@ -0,0 +1,218 @@
+import {Scraper} from "agent-twitter-client";
+import {Cookie} from "tough-cookie";
+import fs from "fs";
+import path from "path";
+import {fileURLToPath} from 'url';
+
+
+// Get the directory name properly in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Create directory if it doesn't exist
+
+
+export default class TwitterService {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+        this.scraper = new Scraper();
+        this.jsonDir = path.join(__dirname, 'json');
+        this.cookiePath = path.join(this.jsonDir, 'cookies.json');
+        this.tweetsDir = this.jsonDir
+        this.isLoggedIn = false;
+    }
+
+    async initialize() {
+        await fs.mkdirSync(path.dirname(this.jsonDir), { recursive: true });
+        try {
+            await this.loadCookies();
+
+            if (!this.isLoggedIn) {
+                console.log('Logging in...');
+                await this.login();
+            }
+            console.log('Twitter service initialized!');
+            return true
+        } catch (error) {
+            console.log('Init error:', error);
+            throw error;
+        }
+    }
+
+    async login() {
+        try {
+            await this.scraper.login(
+                this.username,
+                this.password
+            );
+            this.isLoggedIn = true;
+            const cookies = await this.scraper.getCookies();
+            await this.saveCookies(cookies);
+            console.log('Login successful!');
+        } catch (error) {
+            console.log('Login error:', error);
+            throw error;
+        }
+    }
+
+    async loadCookies() {
+        try {
+            if (fs.existsSync(this.cookiePath)) {
+                const cookiesData = fs.readFileSync(this.cookiePath, 'utf8');
+                const cookiesJson = JSON.parse(cookiesData);
+
+                const cookies = cookiesJson.map(cookieData => {
+                    try {
+                        return Cookie.fromJSON(cookieData).toString(); // without .toString
+                    } catch (error) {
+                        console.error('Failed to parse cookie:', error);
+                        return null;
+                    }
+                }).filter(cookie => cookie !== null);
+
+                if (cookies.length > 0) {
+                    await this.scraper.setCookies(cookies);
+                    this.isLoggedIn = await this.scraper.isLoggedIn();
+                    console.log('Cookies loaded successfully');
+                } else {
+                    console.log('No valid cookies found');
+                    this.isLoggedIn = false;
+                }
+            } else {
+                console.log('No existing cookies found');
+            }
+        } catch (error) {
+            console.error('Cookie loading error:', error);
+            this.isLoggedIn = false;
+        }
+    }
+
+    async saveCookies(cookies) {
+        try {
+            const cookiesJson = cookies.map(cookie => cookie.toJSON());
+            await fs.writeFileSync(this.cookiePath, JSON.stringify(cookiesJson, null, 2));
+            console.log('Cookies saved successfully');
+        } catch (error) {
+            console.error('Cookie saving error:', error);
+        }
+    }
+
+    // async processTweetData(tweet) {
+    //     try {
+    //         console.log('------------------------------')
+    //         console.log(tweet)
+    //         if (!tweet || !tweet.id) return null;
+    //
+    //         let timestamp = tweet.timestamp;
+    //         if (!timestamp) {
+    //             timestamp = tweet.timeParsed?.getTime();
+    //         }
+    //
+    //         if (!timestamp) return null;
+    //
+    //         if (timestamp < 1e12) timestamp *= 1000;
+    //
+    //         if (isNaN(timestamp) || timestamp <= 0) {
+    //             console.log(`Invalid timestamp for tweet ${tweet.id}`);
+    //             return null;
+    //         }
+    //
+    //         return {
+    //             id: tweet.id,
+    //             text: tweet.text,
+    //             username: tweet.username || 'unknown',
+    //             timestamp,
+    //             createdAt: new Date(timestamp).toISOString(),
+    //             isReply: Boolean(tweet.isReply),
+    //             isRetweet: Boolean(tweet.isRetweet),
+    //             likes: tweet.likes || 0,
+    //             retweetCount: tweet.retweets || 0,
+    //             replies: tweet.replies || 0,
+    //             photos: tweet.photos || [],
+    //             videos: tweet.videos || [],
+    //             urls: tweet.urls || [],
+    //             permanentUrl: tweet.permanentUrl,
+    //             quotedStatusId: tweet.quotedStatusId,
+    //             inReplyToStatusId: tweet.inReplyToStatusId,
+    //             hashtags: tweet.hashtags || [],
+    //         };
+    //     } catch (error) {
+    //         console.log(`Error processing tweet ${tweet?.id}: ${error.message}`);
+    //         return null;
+    //     }
+    // }
+
+    async loadTweets(name) {
+        // if tweets file exists, read from file
+        const filePath = path.join(this.tweetsDir, `tweets-by-${name}.json`)
+        if (fs.existsSync(filePath)) {
+            const tweets = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+            console.log(`Total loaded tweets for ${name}: ${tweets.length}`)
+            return tweets;
+        }
+        return null;
+    }
+
+
+    async saveTweets(name, tweetsJson) {
+        if (tweetsJson.length > 0) {
+            try {
+                console.log(`Total tweets to save for ${name}: ${tweetsJson.length}.`)
+                const filePath = path.join(this.tweetsDir, `tweets-by-${name}.json`)
+                console.log(filePath)
+                await fs.writeFileSync(filePath, JSON.stringify(tweetsJson, null, 2));
+                console.log(`Tweets saved successfully`);
+            } catch (error) {
+                console.log('Tweets saving error:', error);
+            }
+        } else {
+            console.log('No tweets to save for ', name);
+        }
+    }
+
+    async getNewTweets(name, count = 10) {
+        // check scraper
+        if (!this.isLoggedIn) {
+            const scraperInitialized = await this.initialize();
+            if (!scraperInitialized) {
+                throw new Error('Failed to initialize Twitter scraper');
+            }
+        }
+
+        // Get tweets using async iterator
+        const tweets = [];
+        const tweetsIterator = this.scraper.getTweets(name, count);
+
+        for await (const tweet of tweetsIterator) {
+            // tweets.push(await this.processTweetData(tweet));
+            tweets.push(tweet);
+        }
+        console.log(`Total fetched tweets by ${name}: ${tweets.length}`);
+        return tweets;
+    }
+
+    async getDiffTweets(name, count = 20) {
+        const oldTweets = await this.loadTweets(name);
+        const newTweets = await this.getNewTweets(name, count);
+
+        if (!oldTweets || oldTweets.length === 0) {
+            console.log('No old tweets found.');
+            await this.saveTweets(name, newTweets);
+            return [];
+        } else if (oldTweets && oldTweets.length > 0 && newTweets.length > 0) {
+            const diffTweets = await this.compareTweets(oldTweets, newTweets);
+            console.log(diffTweets);
+
+            // save new json1 only if new tweet posted.
+            if (diffTweets.length > 0) {
+                await this.saveTweets(name, newTweets);
+            }
+            return diffTweets;
+        }
+    }
+
+    async compareTweets(oldTweets, newTweets) {
+        const oldTweetsIds = oldTweets.map((tweet) => tweet.id);
+        return newTweets.filter(tweet => !oldTweetsIds.includes(tweet.id));
+    }
+}
---
+++ src/bot.js
@@ -0,0 +1,127 @@
+import path from "path";
+import dotenv from 'dotenv';
+import {Bot, InlineKeyboard} from 'grammy'
+import TwitterService from "./TwitterService.js";
+
+const TIME_INTERVAL = 2 * 60 * 1000 // Checks every 2 minutes
+const TIMEOUT = 15 * 60 * 1000 // Checks every 2 minutes
+// const TELEGRAM_MAX_MESSAGE_LENGTH = 4096 // set by telegram
+// const TELEGRAM_MAX_CAPTION_LENGTH = 1024 // set by telegram
+
+// getting private data
+const ROOT_DIRECTORY = path.dirname(import.meta.dirname);
+dotenv.config({path: `${ROOT_DIRECTORY}/.env`});
+
+
+const twitterService = new TwitterService(
+    process.env.TWITTER_USERNAME,
+    process.env.TWITTER_PASSWORD
+);
+
+await twitterService.initialize();
+
+
+const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
+
+try {
+    await bot.api.sendMessage(process.env.TELEGRAM_ADMIN_ID, 'I am starting to work.')
+} catch (error){
+    console.error("Couldn't send welcome message to admin: ", error)
+}
+
+const chatId = process.env.TELEGRAM_CHAT_ID;
+const twitterName = process.env.TWITTER_NAME_TO_FOLLOW
+
+
+setInterval(async () => {
+    // Get tweets by account since the last check
+    try {
+        const newTweets = await twitterService.getDiffTweets(twitterName);
+        newTweets.reverse(); // old tweets first
+        for (const tweet of newTweets) {
+            console.log(tweet)
+            console.log('--- END OF TWEET ---');
+            // Do not forward replies
+            if (tweet.isReply) continue
+
+            const profileUrl = `https://x.com/${twitterName}/`
+            const tweetUrl = `${profileUrl}status/${tweet.id}`
+            const titleType = tweet.isRetweet ? "retweeted 🔁" : tweet.isQuoted ? 'quoted 📝' : "tweeted ✏️"
+            const title = `<a href="${profileUrl}">${twitterName}</a> <b>${titleType}:</b>`
+            let text = `${title}\n\n${tweet.text}`;
+
+            // Replace Telegram alias to Twitter direct links in order to avoid scams
+            const mentions = text.matchAll(/\@(\w+)/g)
+            for (const mention of mentions) {
+                text = text.replace(mention[0], `<a href="https://x.com/${mention[1]}">${mention[0]}</a>`)
+            }
+
+            // construct inlineButton
+            const raidText = '🔥 RAID IT NOW 🚀'
+            const keyboard = new InlineKeyboard().url(raidText, tweetUrl);
+            const otherOptions = {
+                reply_markup: keyboard,
+                parse_mode: 'HTML',
+                message_thread_id: process.env.TELEGRAM_MESSAGE_THREAD_ID,
+                link_preview_options: {
+                    is_disabled: false,
+                    prefer_large_media: true
+                }
+            }
+
+            let sentMessage;
+
+            // Note that we are checking tweet.text, not text. This is because we insert mentions in the text that are no useful urls
+            if (!tweet.isRetweet && !tweet.isQuoted) {
+                otherOptions.caption = text;
+                if ((tweet.photos.length + tweet.videos.length) > 1) { // 2 or more media
+                    const mediaPhotos = tweet.photos.map(media => {
+                        return {
+                            media: media.url,
+                            type: "photo"
+                        }
+                    });
+
+                    const mediaVideos = tweet.videos.map(media => {
+                        return {
+                            media: media.url,
+                            type: "video"
+                        }
+                    });
+
+                    const media = mediaPhotos.concat(mediaVideos).slice(0, 10); // using only first 10
+                    media[0].parse_mode = 'HTML';
+                    media[0].caption = text + `\n\n<a href="${tweetUrl}">${raidText}</a>`;
+                    sentMessage = await bot.api.sendMediaGroup(chatId, media, otherOptions)
+
+                } else {
+                    otherOptions.caption = text
+                    if (tweet.photos.length > 0) {
+                        sentMessage = await bot.api.sendPhoto(chatId, tweet.photos[0].url, otherOptions)
+                    } else if (tweet.videos.length > 0) {
+                        sentMessage = await bot.api.sendVideo(chatId, tweet.videos[0].url, otherOptions)
+                    }
+                }
+            } else {  // usual message
+                if (tweet.isRetweet) {
+                    otherOptions.link_preview_options.url = tweet.retweetedStatus.permanentUrl
+                } else if (tweet.isQuoted) {
+                    otherOptions.link_preview_options.url = tweet.quotedStatus.permanentUrl
+                } else if (tweet.urls.length > 0) {
+                    otherOptions.link_preview_options.url = tweet.urls[0]
+                } else {
+                    otherOptions.link_preview_options.is_disabled = true
+                }
+                sentMessage = await bot.api.sendMessage(chatId, text, otherOptions)
+            }
+            console.log('message sent: ', sentMessage)
+        }
+    } catch (error) {
+        console.error("Got error ", error)
+        console.log("Sleeping ms: ", TIMEOUT)
+        twitterService.isLoggedIn = false;
+        setTimeout(() => {
+            console.log("Lets go ", TIMEOUT);
+        }, TIMEOUT);
+    }
+}, TIME_INTERVAL)
